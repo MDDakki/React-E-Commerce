60a45da190177d0e7ec50fc94c2a322667c5dfc2
MoDakki <mohammaddakki@gmail.com>
2024-01-17 13:06:30 +0100

    Gesamt Preis & Steuer & Sub Total & Menge

--- src/components/Seiten/Wagen.jsx
+++ src/components/Seiten/Wagen.jsx
@@ -16,6 +16,47 @@ const Wagen = () => {
                                 <div className="wagen__menge">Menge</div>
                                 <div className="wagen__gesamt">Preis</div>
                             </div>
+                            <div className="wagen__body">
+                                <div className="wagen__item">
+                                    <div className="wagen__buch">
+                                        <img src="https://covers.openlibrary.org/b/id/2380224-L.jpg" alt="" className="wagen__buch--img" />
+                                    <div className="wagen__buch--info">
+                                        <span className="wagen__buch--titel">
+                                                Rich Dad Poor Dad
+                                        </span>
+                                        <span className="wagen__buch--preis">
+                                        €10.00
+                                        </span>
+                                        <button className="wagen__buch--entfernen">
+                                            Entfernen
+                                        </button>
+                                          </div>
+                                    </div>
+                                    <div className="wagen__menge">
+                                        <input type="number" min={0} max={99} className="wagen__input" />
+                                    </div>
+                                    <div className="wagen__gesamt">
+                                    €10.00
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="gesamt">
+                            <div className="gesamt__item gesamt__sub-total">
+                                <span>Subtotal</span>
+                                <span>€9.00</span>
+                            </div>
+                            <div className="gesamt__item gesamt__steuer">
+                                <span>Steuer</span>
+                                <span>€1.00</span>
+                            </div>
+                            <div className="gesamt__item gesamt__preis">
+                                <span>Gesamt Preis</span>
+                                <span>€1.00</span>
+                            </div>
+                            <button className="taste taste__checkout no-cursor" onClick={() => alert('Noch nicht Implementiert')}>
+                                Fortfahren
+                            </button>
                         </div>
                     </div>
                 </div>
